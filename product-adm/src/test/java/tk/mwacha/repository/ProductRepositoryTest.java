375a20885d547b1bb51e283de7f84c6221ab37dd
mwacha <mwacha@gmail.com>
2023-01-29 19:43:58 -0300

    second commit

--- product-adm/src/test/java/tk/mwacha/repository/ProductRepositoryTest.java
+++ product-adm/src/test/java/tk/mwacha/repository/ProductRepositoryTest.java
@@ -1,6 +1,8 @@
 package tk.mwacha.repository;
 
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 import tk.mwacha.context.ContextTestRepository;
@@ -8,9 +10,12 @@ import tk.mwacha.context.ContextTestRepository;
 @Sql(scripts = {"classpath:/script_sql/clean-data.sql", "classpath:/script_sql/create-product.sql"})
 class ProductRepositoryTest extends ContextTestRepository {
 
-        private static final long DEFAULT_TENANT_ID = 1;
         @Autowired
         private ProductRepository repository;
 
-       // TODO Criar a pasta e script sql
+       // Criar a pasta e script sql
+    @Test
+    void should_create() {
+        Assertions.assertTrue(true);
+    }
 }
